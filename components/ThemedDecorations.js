44d183b7a68ab1d69c73403f24cf2a9d28820cc1
github username <github email address>
2016-11-17 16:09:35 -0500

    Done.

---
+++ components/ThemedDecorations.js
@@ -0,0 +1,18 @@
+const React = require('react');
+
+export default class ThemedDecorations extends React.Component {
+  render(){
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme,
+      });
+    });
+    return(
+      <div>
+        {childrenWithExtraProp}
+      </div>
+    );
+  }
+}
+
+module.exports = ThemedDecorations;
